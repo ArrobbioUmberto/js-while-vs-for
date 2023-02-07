f461378770026ad65ef2f22cf59483e66e16f85f
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-02-07 18:50:14 +0100

    FEAT condition fixed

--- js/app.js
+++ js/app.js
@@ -1,26 +1,28 @@
-console.log ('ciao')
+console.log('ciao')
 
 // Utilizziamo un ciclo per chiedere all’utente un numero tramite prompt. Se il numero è dispari, 
 // lo salviamo in una lista. Continuiamo a chiedere un numero all’utente finchè non avremo ottenuto 10 numeri dispari.
 //  Tutti gli altri valori li scartiamo (numeri pari ed eventuali NaN).
 // Una volta ottenuti tutti i 10 numeri, stampiamoli in console uno alla volta.
 let lista = []
-let ask 
-for (let i = 0; lista.length < 10; i++){
-        while ((ask<0) || (isNaN(ask))) {
-        ask = parseInt(prompt('Dimmi un numero'))
-        // lista.push(askNumber)
+
+for (let i = 0; lista.length < 10; i++) {
+    
+    do {
+        let ask = parseInt(prompt('Dimmi un numero'))
         console.log(ask)
         let pari = ask % 2
         console.log(pari)
-        if(pari != 0){
+        if (pari!== 0 && !isNaN(pari)) {
             lista.push(ask)
             console.log(lista)
         }
+        
     }
-    
+    while (lista.length < 10);
+
 }
 
 
-    
+
 
